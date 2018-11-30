ddf1b0c1a2e7e9145154c9b57db70553ec15c307
haruto.watanabe <w.haru10n@gmail.com>
2018-11-30 23:39:36 +0900

    dpdaFactoryを追加

--- just_add_power/pda_rule.rb
+++ just_add_power/pda_rule.rb
@@ -1,8 +1,8 @@
 class PDARule < Struct.new(:state, :character, :next_state,
                            :pop_character, :push_characters)
   def applies_to?(configuration, character)
-    self.state == configuration.state &&
-      self.pop_character == configuration.stack.top &&
+    state == configuration.state &&
+      pop_character == configuration.stack.top &&
       self.character == character
   end
 
@@ -13,7 +13,7 @@ class PDARule < Struct.new(:state, :character, :next_state,
   def next_stack(configuration)
     popped_stack = configuration.stack.pop
 
-    push_characters.reverse.
-      inject(popped_stack) { |stack, character| stack.push(character) }
+    push_characters.reverse
+                   .inject(popped_stack) { |stack, character| stack.push(character) }
   end
 end
