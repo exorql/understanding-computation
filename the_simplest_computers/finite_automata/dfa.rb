d55d249e1e5134d3ae92662de68372dc7298e7f6
haruto.watanabe <w.haru10n@gmail.com>
2018-12-03 22:47:23 +0900

    typo

--- the_simplest_computers/finite_automata/dfa.rb
+++ the_simplest_computers/finite_automata/dfa.rb
@@ -1,7 +1,7 @@
 class FARule < Struct.new(:state, :character, :next_state)
   def applies_to?(state, character)
     self.state == state && self.character == character
-   end
+  end
 
   def follow
     next_state
@@ -15,7 +15,7 @@ end
 class DFARulebook < Struct.new(:rules)
   def next_state(state, character)
     rule_for(state, character).follow
-   end
+  end
 
   def rule_for(state, character)
     rules.detect { |rule| rule.applies_to?(state, character) }
@@ -25,23 +25,23 @@ end
 class DFA < Struct.new(:current_state, :accept_states, :rulebook)
   def accepting?
     accept_states.include?(current_state)
-   end
+  end
 
   def read_character(character)
     self.current_state = rulebook.next_state(current_state, character)
-    end
+  end
 
   def read_string(string)
     string.chars.each do |character|
       read_character(character)
     end
-   end
+  end
 end
 
 class DFADesign < Struct.new(:start_state, :accept_states, :rulebook)
   def to_dfa
     DFA.new(start_state, accept_states, rulebook)
-   end
+  end
 
   def accepts?(string)
     to_dfa.tap { |dfa| dfa.read_string(string) }.accepting?
