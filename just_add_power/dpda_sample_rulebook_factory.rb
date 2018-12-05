490ca10ac9c3b8101c6bd0a04a340506d0bb4f50
haruto.watanabe <w.haru10n@gmail.com>
2018-12-05 21:03:29 +0900

    factoryを追加

--- just_add_power/dpda_sample_rulebook_factory.rb
+++ just_add_power/dpda_sample_rulebook_factory.rb
@@ -7,4 +7,16 @@ class DPDASampleRulebookFactory
                        PDARule.new(2, nil, 1, '$', ['$'])
                      ])
   end
+
+  def self.build_second
+    rulebook = DPDARulebook.new([
+                                  PDARule.new(1, 'a', 2, '$', ['a', '$']),
+                                  PDARule.new(1, 'b', 2, '$', ['b', '$']),
+                                  PDARule.new(2, 'a', 2, 'a', %w[a a]),
+                                  PDARule.new(2, 'b', 2, 'b', %w[b b]),
+                                  PDARule.new(2, 'a', 2, 'b', []),
+                                  PDARule.new(2, 'b', 2, 'a', []),
+                                  PDARule.new(2, nil, 1, '$', ['$'])
+                                ])
+  end
 end
